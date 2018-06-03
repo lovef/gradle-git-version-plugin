17ad95f85a25fe904e505db4313a679211134936
Love F <lovef.code@gmail.com>
2018-06-03 15:10:11 +0200

    improved configuration

--- gradle-git-version-plugin/src/main/kotlin/se/lovef/gradle/GradleGitVersionPlugin.kt
+++ gradle-git-version-plugin/src/main/kotlin/se/lovef/gradle/GradleGitVersionPlugin.kt
@@ -1,5 +1,6 @@
 package se.lovef.gradle
 
+import groovy.lang.Closure
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import se.lovef.git.GitVersion
@@ -10,28 +11,64 @@ import se.lovef.git.GitVersion
  */
 class GradleGitVersionPlugin : Plugin<Project> {
 
+    class Config(
+        private val project: Project
+    ) : GitVersion.Config {
+        lateinit var gitVersionExtension: GitVersionExtension
+        override val baseVersion: String
+            get() = project.properties["baseVersion"] as? String ?: gitVersionExtension.baseVersion
+    }
+
     override fun apply(project: Project) {
-        val gitVersion = GitVersion(GradleGitExecutor(project), project.properties["baseVersion"] as String)
+        val config = Config(project)
+        val gitVersion = GitVersion(GradleGitExecutor(project), config)
+        val gitVersionExtension = project.extensions.create(
+            "gitVersion", GitVersionExtension::class.java,
+            this, project, gitVersion
+        )
+        config.gitVersionExtension = gitVersionExtension
         project.version = gitVersion.version
-        project.extensions.extraProperties["gitVersion"] = ExternalGitVersion(gitVersion)
         val tagTask = project.tasks.create("tag", TagTask::class.java) {
             it.gitVersion = gitVersion
         }
         project.tasks.create("publishTag", PublishTagTask::class.java) {
             it.gitVersion = gitVersion
-            it.shouldRunAfter(tagTask)
+            it.mustRunAfter(tagTask)
         }
     }
 
-    @Suppress("MemberVisibilityCanBePrivate")
-    class ExternalGitVersion(private val gitVersion: GitVersion) {
+    open class GitVersionExtension(
+        owner: GradleGitVersionPlugin,
+        private val project: Project,
+        private val gitVersion: GitVersion
+    ) : Closure<String>(owner) {
+
+        var baseVersion = "0.0"
+            set(value) {
+                field = value
+                project.version = version
+            }
+
         val version get() = gitVersion.version
+
         val tag get() = gitVersion.tag
+
+        fun doCall(baseVersion: String) = invoke(baseVersion)
+
+        fun doCall(properties: Map<String, String?>): String {
+            properties["baseVersion"]?.let { baseVersion = it }
+            return version
+        }
+
+        operator fun invoke(baseVersion: String): String {
+            this.baseVersion = baseVersion
+            return version
+        }
 
         override fun toString(): String {
             return """{ version: "$version", tag: ${tag.jsonValue} }"""
         }
 
-        private val String?.jsonValue get() = this?.let { "\"$it\"" }
+        private val kotlin.String?.jsonValue get() = this?.let { "\"$it\"" }
     }
 }
